142486884cc6e40b9ee3b713bf7c38107b1a4f6f
Paulo Raoni <raoni_jkd@hotmail.com>
2021-03-09 18:33:12 -0300

    Alterando local das funções

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const soma = (a, b) => a + b
-
-const multiplica = (a, b) => a * b
-
 const main = () => {
+    const soma = (a, b) => a + b
+    const multiplica = (a, b) => a * b
     console.log(soma())    
     console.log(multiplica())
 }
