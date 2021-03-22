aa273647f56c2145a945caabca869cb3f9f62237
Paulo Raoni <raoni_jkd@hotmail.com>
2021-03-22 20:45:14 -0300

    adicionando constantes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+const A = 30, B = 10;
+
 const soma = (a, b) => a + b
 const multiplica = (a, b) => a * b
 
-
-const main = () => {
-    
-    console.log(soma())    
-    console.log(multiplica())
+const main = () => {    
+    console.log(soma(A, B))    
+    console.log(multiplica(A, B))
 }
 
 main()
