17164874207dcfa5676378f1b3388ff045dde80d
Paulo Raoni <raoni_jkd@hotmail.com>
2021-03-22 21:46:19 -0300

    desfazendo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const A = 30, B = 10;
+const A = 30, B = 10
 
 const soma = (a, b) => a + b
 const multiplica = (a, b) => a * b
@@ -6,8 +6,6 @@ const multiplica = (a, b) => a * b
 const main = () => {    
     console.log(soma(A, B))    
     console.log(multiplica(A, B))
-    console.log(subtrai(A, B))
-    console.log(divide(A, B))
 }
 
 main()
